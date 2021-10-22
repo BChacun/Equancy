18f7cb5a028163d200b66c1588a5d80ba49367ba
Baptiste Chacun <baptiste.chacun@imt-atlantique.net>
2021-10-22 09:57:57 +0200

    Base

---
+++ Streamlit/Uber.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+
+
+
+DATE_COLUMN = 'timestamp'
+LOC = (r'C:\Users\bapti\Documents\Equancy\Machine learning\Comparaison Biblio\Days.csv')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(LOC, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache)")
+
+
+
+st.subheader('Consommation de Caen Journalière')
+st.write(data)
+
+
+
+# Histrogramme :
+    
+st.subheader('Consommation de Caen par An')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.day, bins=3, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+
+
+# Map (Dinguerie pour picard) :
+#st.subheader('Map of all pickups')
+#st.map(data)
+
+
+
+
+
+#streamlit run C:\Users\bapti\Documents\Equancy\Streamlit\Uber.py
